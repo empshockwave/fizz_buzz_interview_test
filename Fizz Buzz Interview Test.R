5f0e2e1fc7e137f87557ee2be2c6a7b218670501
Justin Meyer <meyerjustinm@gmail.com>
2015-10-29 09:04:26 -0500

    A solution to the classic Fizz Buzz interview test in R.
    A solution to the classic Fizz Buzz interview test in R.

---
+++ Fizz Buzz Interview Test.R
@@ -0,0 +1,50 @@
+# Fizz Buzz Test
+
+# Print the numbers from 1 to 100
+# For multiples of 3 print "Fizz"
+# For multiples of 5 print "Buzz"
+# For multiples of both print "FizzBuzz"
+
+# Print the numbers from 1 to 100
+
+for (i in 1:100) {
+        print(i)
+}
+
+# Print the numbers from 1 to 100
+# For multiples of 3 print "Fizz"
+i <- 1
+for (i in 1:100) {
+        if(i %% 3 == 0) {print("Fizz")}
+        else print(i)
+}
+
+# Print the numbers from 1 to 100
+# For multiples of 3 print "Fizz"
+# For multiples of 5 print "Buzz"
+
+for (i in 1:100) {
+        if (i %% 3 == 0) {print("Fizz")}
+        else if (i %% 5 == 0) {print("Buzz")}
+        else print(i)
+}
+
+
+# Print the numbers from 1 to 100
+# For multiples of 3 print "Fizz"
+# For multiples of 5 print "Buzz"
+# For multiples of both print "FizzBuzz"
+
+for (i in 1:100) {
+        if (i %% 3 == 0 & i %% 5 == 0) {print("FizzBuzz")}
+        else if (i %% 3 == 0) {print("Fizz")}
+        else if (i %% 5 == 0) {print("Buzz")}
+        else print(i)
+}
+
+# vectorized fizzbuzz
+m <- 1:100
+m[which(m%%5==0)][which(m%%3==0)] <- "fizzbuzz"
+m[which(as.numeric(m)%%3==0)] <- "fizz"
+m[which(as.numeric(m)%%5==0)] <- "buzz"
+print(m)
